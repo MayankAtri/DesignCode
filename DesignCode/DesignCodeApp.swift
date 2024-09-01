9b935dbddcc97ecd0720f53b023026a2af8890ee
MayankAtri <atrimayank110@gmail.com>
2024-09-01 20:26:13 +0530

    Initial Commit

---
+++ DesignCode/DesignCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeApp.swift
+//  DesignCode
+//
+//  Created by Mayank Atri on 01/09/24.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
